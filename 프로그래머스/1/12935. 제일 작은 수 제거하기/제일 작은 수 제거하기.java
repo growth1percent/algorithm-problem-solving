49b0e526a4ebc5f9ccd9e05e1ebdabac67685875
Sangheon Lee <ryrymel35@naver.com>
2025-04-12 15:05:58 +0900

    Update 제일 작은 수 제거하기.java

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,8 +1,5 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr) {
-        /*
         int min = arr[0];
         
         for (int i = 1; i < arr.length; i++) {
@@ -11,35 +8,13 @@ class Solution {
             }
         }
         
-        ArrayList<Integer> answer = new ArrayList<>();
+        int idx = 0;
+        int[] answer = new int[arr.length - 1];
         
         for (int value : arr) {
             if (value != min) {
-                answer.add(value);
+                answer[idx++] = value;
             }
-        }
-        
-        return arr.length <= 1 ? new int[] {-1} : answer.stream().mapToInt(Integer::intValue).toArray();
-        */
-        
-        int min = arr[0];
-        int minIdx = 0;
-        
-        for (int i = 1; i < arr.length; i++) {
-            if (min > arr[i]) {
-                min = arr[i];
-                minIdx = i;
-            }
-        }
-        
-        int[] answer = new int[arr.length - 1];
-        int idx = 0;
-        
-        for (int i = 0; i < arr.length; i++) {
-            if (i == minIdx) {
-                continue;
-            }
-            answer[idx++] = arr[i];
         }
         
         return arr.length <= 1 ? new int[] {-1} : answer;
