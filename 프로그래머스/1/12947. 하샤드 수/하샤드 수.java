792b5dd574ecdaa408520f687dd627fd6cbdcbc6
Sangheon Lee <ryrymel35@naver.com>
2025-04-13 16:32:36 +0900

    [level 1] Title: 하샤드 수, Time: 0.19 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
-        String str = Integer.toString(x);
+        String[] str = Integer.toString(x).split("");
         int value = 0;
         
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            int num = c - '0';
-            value += num;
+        for (int i = 0; i < str.length; i++) {
+            value += Integer.parseInt(str[i]);
         }
+        
         if (x % value == 0) {
             answer = true;
         } else {
