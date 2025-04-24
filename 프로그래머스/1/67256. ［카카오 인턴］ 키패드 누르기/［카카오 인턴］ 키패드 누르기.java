2e5ff6709dbb645d6f241afd01450579afe9a47f
Sangheon Lee <ryrymel35@naver.com>
2025-04-24 23:37:14 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.37 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -13,31 +13,31 @@ class Solution {
         int[] rightPos = {3, 2};
         
         for (int num : numbers) {
+            int[] target = keypad[num];
+            
             if (num == 1 || num == 4 || num == 7) {
+                leftPos = target;
                 answer.append("L");
-                leftPos = keypad[num];
             } else if (num == 3 || num == 6 || num == 9) {
+                rightPos = target;
                 answer.append("R");
-                rightPos = keypad[num];
             } else {
-                int[] target = keypad[num];
-                
                 int leftDist = Math.abs(leftPos[0] - target[0]) + Math.abs(leftPos[1] - target[1]);
                 int rightDist = Math.abs(rightPos[0] - target[0]) + Math.abs(rightPos[1] - target[1]);
                 
                 if (leftDist < rightDist) {
-                    answer.append("L");
                     leftPos = target;
+                    answer.append("L");
                 } else if (rightDist < leftDist) {
-                    answer.append("R");
                     rightPos = target;
+                    answer.append("R");
                 } else {
-                    if (hand.equals("right")) {
-                        answer.append("R");
-                        rightPos = target;
-                    } else {
-                        answer.append("L");
+                    if (hand.equals("left")) {
                         leftPos = target;
+                        answer.append("L");
+                    } else {
+                        rightPos = target;
+                        answer.append("R");
                     }
                 }
             }
