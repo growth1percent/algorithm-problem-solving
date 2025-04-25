7dd24ee5f08e1ad1f7e20a1c9fb3e28c4eec2407
Sangheon Lee <ryrymel35@naver.com>
2025-04-25 16:09:53 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,13 +1,12 @@
 class Solution {
     public int solution(int n) {
+        int answer = 0;
         StringBuilder sb = new StringBuilder();
         
         while (n > 0) {
             sb.append(n % 3);
             n /= 3;
         }
-        
-        int answer = 0;
         
         for (int i = 0; i < sb.length(); i++) {
             answer += (sb.charAt(i) - '0') * Math.pow(3, sb.length() - 1 - i);
