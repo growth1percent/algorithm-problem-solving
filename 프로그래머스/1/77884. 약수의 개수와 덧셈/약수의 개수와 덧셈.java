af10f223099ad7e5d9c86d24b3969f8f3980435d
Sangheon Lee <ryrymel35@naver.com>
2025-04-27 16:47:19 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.09 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,18 +1,14 @@
 class Solution {
     public int solution(int left, int right) {
+        // 어떤 수의 약수 개수가 홀수인 경우는 그 수가 **정수 제곱수(perfect square)**일 때 뿐이다.
         int answer = 0;
         
         for (int i = left; i <= right; i++) {
-            int count = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    count++;
-                }
-            }
-            if (count % 2 == 0) {
-                answer += i;
-            } else {
+            int sqrt = (int) Math.sqrt(i);
+            if (sqrt * sqrt == i) {
                 answer -= i;
+            } else {
+                answer += i;
             }
         }
         
