d9439922286f0b2662b74385d07c4b3189a7ffc6
Sangheon Lee <ryrymel35@naver.com>
2025-04-30 22:48:28 +0900

    [level 1] Title: 신고 결과 받기, Time: 292.66 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -5,30 +5,38 @@ class Solution {
         // 중복 제거
         Set<String> set = new HashSet<>(Arrays.asList(report));
         
-        // 신고 당한 사람, 신고자 목록 매핑
+        // 신고당한 사람, 신고자 해시 맵
         Map<String, Set<String>> reportedBy = new HashMap<>();
         
         for (String s : set) {
             String[] array = s.split(" ");
-            reportedBy.putIfAbsent(array[1], new HashSet<>());
-            reportedBy.get(array[1]).add(array[0]);
+            String reporter = array[0];
+            String reported = array[1];
+            
+            reportedBy.putIfAbsent(reported, new HashSet<>());
+            reportedBy.get(reported).add(reporter);
         }
         
-        // 정지 대상 찾기 (k번 이상 신고당한 사람)
+        // 정지된 유저 해시 셋
         Set<String> bannedUsers = new HashSet<>();
-        for (String user : reportedBy.keySet()) {
-            if (reportedBy.get(user).size() >= k) {
-                bannedUsers.add(user);
+        for (String reported : reportedBy.keySet()) {
+            if (reportedBy.get(reported).size() >= k) {
+                bannedUsers.add(reported);
             }
         }
         
-        int[] answer = new int[id_list.length];
+        // 인덱스 해시 맵
+        Map<String, Integer> indexMap = new HashMap<>();
         for (int i = 0; i < id_list.length; i++) {
             String reporter = id_list[i];
-            for (String banned : bannedUsers) {
-                if (reportedBy.get(banned).contains(reporter)) {
-                    answer[i]++;
-                }
+            indexMap.put(reporter, i);
+        }
+        
+        int[] answer = new int[id_list.length];
+        for (String banned : bannedUsers) {
+            for (String reporter : reportedBy.get(banned)) {
+                int idx = indexMap.get(reporter);
+                answer[idx]++;
             }
         }
         
