d33d74eae0dbec64b35f6fc5dd3f488dc0c7a24b
Sangheon Lee <ryrymel35@naver.com>
2025-05-02 12:45:25 +0900

    [level 1] Title: 콜라 문제, Time: 0.12 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -3,8 +3,9 @@ class Solution {
         int answer = 0;
         
         while (n >= a) {
-            answer += (n / a) * b;
-            n = (n / a) * b + n % a;
+            int exchange = (n / a) * b;
+            answer += exchange;
+            n = exchange + n % a;
         }
         
         return answer;
