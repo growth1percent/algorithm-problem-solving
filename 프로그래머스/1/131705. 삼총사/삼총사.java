73ca3b94084cdbde7bf79012dbe3528b646d8fa7
Sangheon Lee <ryrymel35@naver.com>
2025-05-02 12:27:20 +0900

    [level 1] Title: 삼총사, Time: 0.49 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -3,14 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
+        int n = number.length;
         Arrays.sort(number);
         
-        for (int i = 0; i < number.length; i++) {
-            for (int j = i + 1; j < number.length; j++) {
-                for (int k = j + 1; k < number.length; k++) {
+        for (int i = 0; i < n-2; i++) {
+            if (number[i] > 0) break;
+            
+            for (int j = i + 1; j < n-1; j++) {
+                if (number[i] + number[j] > 0) break;
+                
+                for (int k = j + 1; k < n; k++) {
                     if (number[i] + number[j] + number[k] == 0) {
                         answer++;
-                    }
+                    } else if (number[i] + number[j] + number[k] > 0) break;
                 }
             }
         }
