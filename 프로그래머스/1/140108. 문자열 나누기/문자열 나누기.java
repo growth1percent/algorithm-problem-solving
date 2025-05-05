ec4b71b35d744640e877f592b93b2adcfc9a1019
Sangheon Lee <ryrymel35@naver.com>
2025-05-05 15:56:39 +0900

    [level 1] Title: 문자열 나누기, Time: 1.08 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,31 +1,28 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        char[] array = s.toCharArray();
-        int idx = 0;
+        int count = 0;
+        char target = s.charAt(0);
         
-        while (idx < s.length()) {
-            int sameCount = 1;
-            int diffCount = 0;
-            char c = array[idx];
-            for (int i = idx + 1; i < s.length(); i++) {
-                if (c == array[i]) {
-                    sameCount++;
-                } else {
-                    diffCount++;
-                }
-                
-                if (sameCount == diffCount) {
-                    answer++;
-                    idx = i + 1;
-                    break;
-                }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == target) {
+                count++;
+            } else {
+                count--;
             }
-            if (sameCount != diffCount) {
+            
+            if (count == 0) {
                 answer++;
-                break;
+                if (i + 1 < s.length()) {
+                    target = s.charAt(i + 1);   
+                }
             }
         }
+        
+        if (count > 0) {
+            answer++;
+        }
+        
         return answer;
     }
 }
