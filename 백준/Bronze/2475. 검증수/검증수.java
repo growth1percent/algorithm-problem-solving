04b56f8830e50463386c36c38f8fd6a6e120e55a
Sangheon Lee <ryrymel35@naver.com>
2025-05-08 01:35:58 +0900

    [Bronze V] Title: 검증수, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int sum = 0;
+
+            for (int i = 0; i < 5; i++) {
+                int num = sc.nextInt();
+                sum += Math.pow(num, 2);
+            }
+
+            int checkDigit = sum % 10;
+            System.out.println(checkDigit);
+    }
+}
