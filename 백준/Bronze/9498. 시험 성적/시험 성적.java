385025a86e9d66394ff0fa0d8992496d89c220e3
Sangheon Lee <ryrymel35@naver.com>
2025-05-12 23:21:43 +0900

    [Bronze V] Title: 시험 성적, Time: 184 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int score = sc.nextInt();
+       char answer;
+
+       if (score >= 90) {
+           answer = 'A';
+       } else if (score >= 80) {
+           answer = 'B';
+       } else if (score >= 70) {
+           answer = 'C';
+       } else if (score >= 60) {
+           answer = 'D';
+       } else {
+           answer = 'F';
+       }
+
+       System.out.println(answer);
+    }
+}
