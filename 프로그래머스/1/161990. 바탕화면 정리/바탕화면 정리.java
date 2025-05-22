c905382bf0a080f80f5d1f287e109597c8089fc3
Sangheon Lee <ryrymel35@naver.com>
2025-05-22 16:20:28 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.45 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,35 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int[] answer = new int[4];
-        int[] min = {wallpaper.length, wallpaper[0].length()};
-        int[] max = {0, 0};
+        int minRow = wallpaper.length;
+        int minCol = wallpaper[0].length();
+        int maxRow = 0;
+        int maxCol = 0;
         
         for (int i = 0; i < wallpaper.length; i++) {
-                String s = wallpaper[i];
-                char[] array = s.toCharArray();
-            for (int j = 0; j < wallpaper[0].length(); j++) {
-                if (array[j] == '#') {
-                    if (min[0] > i) {
-                        min[0] = i;
-                    }
-                    if (min[1] > j) {
-                        min[1] = j;
-                    }
-                    if (max[0] < i + 1) {
-                        max[0] = i + 1;
-                    }
-                    if (max[1] < j + 1) {
-                        max[1] = j + 1;
-                    }
-                } 
+            for (int j = 0; j < wallpaper[i].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    minRow = Math.min(minRow, i);
+                    minCol = Math.min(minCol, j);
+                    maxRow = Math.max(maxRow, i + 1);
+                    maxCol = Math.max(maxCol, j + 1);
+                }
             }
         }
         
-        answer[0] = min[0];
-        answer[1] = min[1];
-        answer[2] = max[0];
-        answer[3] = max[1];
-        
-        return answer;
+        return new int[] {minRow, minCol, maxRow, maxCol};
     }
 }
