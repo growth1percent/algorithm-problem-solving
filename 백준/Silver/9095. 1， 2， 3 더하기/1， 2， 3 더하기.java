c1b1ebb35de9f0d76d5885419870c64881d7015d
Sangheon Lee <ryrymel35@naver.com>
2025-07-01 22:14:02 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 136 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[12];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        for (int i = 0; i < T; i++) {
+            int num = Integer.parseInt(br.readLine());
+            bw.write(dp[num] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
