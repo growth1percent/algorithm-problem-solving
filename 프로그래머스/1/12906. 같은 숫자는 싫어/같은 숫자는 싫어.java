e084014a541dfc922332373834458c2a9a4cc603
Sangheon Lee <ryrymel35@naver.com>
2026-05-18 16:58:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 22.78 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,22 +1,19 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
+    public int[] solution(int[] arr) {
+        Deque<Integer> queue = new ArrayDeque<>();
         
-        stack.push(arr[0]);
-        
-        for (int i = 1; i < arr.length; i++) {
-            if (stack.peek() != arr[i]) {
-                stack.push(arr[i]);
+        for (int i : arr) {
+            if (queue.isEmpty()) {
+                queue.addLast(i);
             } else {
-                continue;
+                if (queue.peekLast() != i) {
+                    queue.addLast(i);   
+                }
             }
         }
-        
-        ArrayList<Integer> reversedList = new ArrayList<>(stack);
-        Collections.reverse(reversedList);
-        
-        return reversedList.stream().mapToInt(Integer::intValue).toArray();
+
+        return queue.stream().mapToInt(Integer::intValue).toArray();
     }
 }
